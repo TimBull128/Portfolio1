6bdd3f8497e0645b57ef59d1bd037b4f24b1b43a
TimBull128 <TimBull128@live.co.uk>
2025-07-24 10:59:54 +0100

    Moving Functions and ViewModels
    - Moving Functions into Relevant areas for later review
    - Moving ViewModels into Relavent areas for later review
    - Note: Need to review ServiceLayer.Base - not left, has repercussions for <Roster>.getSingle(ID)

---
+++ BB2020MVC/Models/BusinessLayer/BusinessRosters.cs
@@ -0,0 +1,39 @@
+﻿using BB2020MVC.Controllers;
+
+using BB2020MVC.Models.ServiceLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BB2020MVC.Models.BusinessLayer
+{
+    public interface IBusinessRosters
+    {
+
+    }
+    public class BusinessRosters : IBusinessRosters
+    {
+        internal ISRosters _Rosters_repo;
+        public BusinessRosters() : this(new ServiceLayer.Rosters) { }
+        public BusinessRosters(ISRosters RosterRepos)
+        {
+            _Rosters_repo = RosterRepos;
+        }
+        TODO: Review and update
+        public void AddRoster(User_Rosters Roster)
+        {
+            Roster = _Rosters_repo.Create(Roster);
+
+            for (int i = 1; i <= 16; i++)
+            {
+                _DataContext.User_Rosters_Players.Add(this.NewRosterPlayer(Roster.RosterID, Roster.RaceID));
+            }
+
+        }
+    }
+}
--- BB2020MVC/Models/BusinessLayer/Defaults.cs
+++ BB2020MVC/Models/BusinessLayer/Defaults.cs
@@ -76,5 +76,89 @@ namespace BB2020MVC.Models.BusinessLayer
 
             };
         }
+        public Rules_LvlType Rules_LvlType()
+        {
+            return new Rules_LvlType()
+            {
+                ID = 0,
+                Description = "",
+                Cost = 0
+            };
+        }
+        public Rules_Skills_List Rules_Skills_List()
+        {
+            return new Rules_Skills_List()
+            {
+                SkillID = 0,
+                Description = "",
+                Name = "",
+                NotOptional = false,
+                SkillTypeID = -1
+            };
+            return NewSkillBase;
+        }
+        public Rules_Skills_Types GetNewSkillTypeBase()
+        {
+            Rules_Skills_Types NewSkillType = new Rules_Skills_Types()
+            {
+                STypeID = GetNewSkillTypeID(),
+                Name = "",
+                Description = ""
+            };
+            return NewSkillType;
+        }
+        public Rules_SpecialRules GetNewSpecialRuleBase()
+        {
+            Rules_SpecialRules NewSpecialRule = new Rules_SpecialRules()
+            {
+                SRID = GetNewSpecialRuleID(),
+                Name = "",
+                Description = ""
+            };
+            return NewSpecialRule;
+        }
+        public Rules_InjuryTypes GetNewInjuryTypesBase()
+        {
+            return new Rules_InjuryTypes
+            {
+                InjuryID = GetNewInjuryTypeID(),
+                Description = "",
+                Name = ""
+            };
+        }
+        public Rules_Skills_FSkills GetNewFSkillBase(int SkillID) { }
+        public User_Rosters NewRoster(int RaceID)
+        {
+            return new User_Rosters
+            {
+                RosterID = GetNewRosterID(),
+                Apoths = false,
+                CheerLeadersQTY = 0,
+                RaceID = RaceID,
+                RerollsQTY = 0,
+                CoachesQTY = 0,
+                InTourney = false,
+                Name = "",
+                Treasury = 0,
+                TV = 0
+
+            };
+        }
+        public User_Rosters_Players NewRosterPlayer(int RosterID, int RaceID)
+        {
+            var Player =
+                (from item in GetPlayersByRaceID(RaceID)
+                 where item.Name == "NA"
+                 select item).First();
+
+            return new User_Rosters_Players
+            {
+                RPlayerID = GetNewRosterPlayerID(),
+                PlayerID = Player.PlayerID,
+                Name = "",
+                RosterID = RosterID,
+                SPP = 0
+            };
+        }
     }
 }
