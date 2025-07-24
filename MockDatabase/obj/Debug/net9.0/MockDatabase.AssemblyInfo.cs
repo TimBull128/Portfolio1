efd9ce0d301989a6978e984ae1cc04a91e28581e
TimBull128 <TimBull128@live.co.uk>
2025-07-24 11:55:55 +0100

    Update After NugetUpdates
    - Nuget identified issues - updated
    - Added MockDatabase to add Unit Tests

--- MockDatabase/obj/Debug/net9.0/MockDatabase.AssemblyInfo.cs
+++ MockDatabase/obj/Debug/net9.0/MockDatabase.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MockDatabase")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27b59e44cd448788aa6b092c010726b1bcb86572")]
 [assembly: System.Reflection.AssemblyProductAttribute("MockDatabase")]
