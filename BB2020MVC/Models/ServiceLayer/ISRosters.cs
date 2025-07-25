e293e78e57b972f449240c87c7a85525190e4759
TimBull128 <TimBull128@live.co.uk>
2025-07-25 12:27:26 +0100

    Update Rosters Service Layer - Remove References to Models.BB2020 - the current location is in DataLayer

--- BB2020MVC/Models/ServiceLayer/ISRosters.cs
+++ BB2020MVC/Models/ServiceLayer/ISRosters.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,38 +11,40 @@ namespace BB2020MVC.Models.ServiceLayer
 {
     public interface ISRosters : IDataLayer<User_Rosters>
     {
-        IList<User_Rosters> All(int RosterID = 0);
+        IList<User_Rosters> All();
         User_Rosters Single(int RosterID);
 
     }
     public interface ISRostersInjury : IDataLayer<User_Rosters_Injury>
     {
         IList<User_Rosters_Injury> All();
+        User_Rosters_Injury Single(int RInjuryID);
     }
     public interface ISRostersLevelTypes : IDataLayer<User_Rosters_LvlTypes>
     {
         IList<User_Rosters_LvlTypes> All();
+        User_Rosters_LvlTypes Single(int RLevelTypeID);
     }
     public interface ISRostersPlayers : IDataLayer<User_Rosters_Players> 
     {
         IList<User_Rosters_Players> All();
+        User_Rosters_Players Single(int RPlayerID);
     }
     public interface ISRostersSkills : IDataLayer<User_Rosters_Skills>
     {
         IList<User_Rosters_Skills> All();
+        User_Rosters_Skills Single(int RPlayerID);
     }
     public class Rosters : DataLayer<User_Rosters>, ISRosters
     {
         public Rosters() => SetTable(DataContext().User_Rosters);
-        public IList<User_Rosters> All(int RosterID = 0)
+        public IList<User_Rosters> All()
         {
-            return (from item in Read() 
-                    where (RosterID == 0 || item.RosterID.Equals(RosterID) )
-                    select item).ToList();
+            return Read();
         }
         public User_Rosters Single(int RosterID)
         {
-            return All(RosterID).First();
+            return (from item in Read() where item.RosterID.Equals(RosterID) select item).First();
         }
     }
     public class RostersInjury : DataLayer<User_Rosters_Injury>, ISRostersInjury
@@ -51,6 +54,10 @@ namespace BB2020MVC.Models.ServiceLayer
         {
             return Read();
         }
+        public User_Rosters_Injury Single(int RInjuryID)
+        {
+            return (from item in Read() where item.RInjuryID.Equals(RInjuryID) select item).First();
+        }
     }
     public class RostersLevelTypes : DataLayer<User_Rosters_LvlTypes>, ISRostersLevelTypes
     {
@@ -58,6 +65,10 @@ namespace BB2020MVC.Models.ServiceLayer
         public IList<User_Rosters_LvlTypes> All()
         {
             return Read();
+        }
+        public User_Rosters_LvlTypes Single(int RLevelTypeID)
+        {
+            return (from item in Read() where item.RLevelTypeID.Equals(RLevelTypeID) select item).First();
         }
     }
     public class RostersPlayers : DataLayer<User_Rosters_Players>, ISRostersPlayers
@@ -67,6 +78,10 @@ namespace BB2020MVC.Models.ServiceLayer
         {
             return Read();
         }
+        public User_Rosters_Players Single(int RPlayerID)
+        {
+            return (from item in Read() where item.RPlayerID.Equals(RPlayerID) select item).First();
+        }
     }
     public class RostersSkills : DataLayer<User_Rosters_Skills>, ISRostersSkills
     {
@@ -74,6 +89,10 @@ namespace BB2020MVC.Models.ServiceLayer
         public IList<User_Rosters_Skills> All()
         {
             return Read();
+        }
+        public User_Rosters_Skills Single(int RPlayerID)
+        {
+            return (from item in Read() where item.RSkillID.Equals(RPlayerID) select item).First();
         }
     }
 }
